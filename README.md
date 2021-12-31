5c9c45fb565dbf766ce65ab2df300d2e57a9b951
Tom Wilson <wilsonte@umich.edu>
2021-12-31 16:38:01 -0500

    create install.sh

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
-# mdi
-Installation utility for the Michigan Data Interface
+# Michigan Data Interface
+
+The [Michigan Data Interface](https://midataint.github.io/) (MDI) 
+is a framework for developing, installing and running a variety of 
+HPC data analysis pipelines and interactive R Shiny data visualization 
+applications within a standardized design and implementation interface.
+
+## Repository contents
+
+Data analysis in the MDI is logically separated into 
+[two stages of code execution](https://midataint.github.io/docs/analysis-flow/) 
+called Stage 1 HPC **pipelines** and Stage 2 web applications (i.e., **apps**).
+Collectively, pipelines and apps are referred to as **tools**.
+
+This repository carries a single **installation script** that will 
+set up the MDI on your computer with all required components and a proper
+folder structure to support all available tools.
+
+Please read the [MDI documentation](https://midataint.github.io/) for 
+more information.
+
+## System requirements
+
+The script in this repository offers two installation modes, with 
+differing requirements.
+
+### Stage 1 Pipelines - Git dependency only
+
+Some users might only run Stage 1 pipelines on an HPC server.
+Selecting this installation mode will skip Stage 2 apps 
+and directly clone the required MDI repositories. Accordingly, Git must 
+be installed on the host machine. See:
+
+- <https://git-scm.com/>
+
+### Stage 2 Pipelines plus Stage 2 Apps - additional R dependency
+
+Stage 2 web apps are R Shiny programs. Accordingly, 
+R must be installed on the host machine to use them. See:
+
+- <https://www.r-project.org/>
+
+We recommend updating to the latest stable R release prior
+to installing the MDI, as MDI installations are tied to specific 
+releases of R (hint: you can install multiple R versions on your 
+computer).
+
+Even if R will be used to complete the MDI installation, Git is still
+required to download this installer repository to your computer.
+
+### Running on Windows - Git Bash
+
+The MDI installation utility is a bash script. To use it on Windows, 
+please install and use Git Bash to expose an appropriate terminal.
+
+- <https://gitforwindows.org/>
+
+Even better, bypass the utility in this repository 
+entirely by creating and downloading a customized installation script here:
+
+- <https://wilsonte-umich.shinyapps.io/mdi-script-generator>
+
+## Install the MDI framework(s)
+
+To clone this repository and run the MDI installation script, execute
+the following from a command shell.
+
+```bash
+git clone https://github.com/MiDataInt/mdi.git
+cd mdi
+./install.sh
+```
+
+Please read the menu options and confirm your installation choice.
+A full installation, including Stage 2 apps, will take many minutes 
+to complete.
+
+## Configure and install tool suites
+
+<code>install.sh</code> clones MDI repositories
+that define the pipeline and apps frameworks, but few actual
+tools. To install tools from any provider, run the following from the
+command line:
+
+```bash
+mdi add https://github.com/GIT_USER/SUITE_NAME-mdi-tools.git
+mdi add GIT_USER/SUITE_NAME-mdi-tools # either format works
+```
+
+Alternatively, you can edit suites.yml and install new suites
+from within the Stage 2 web server.
+
+## Run a Stage 1 pipeline from the command line
+
+The installation process above added the 'mdi' utility
+to your MDI installation folder. You can use it to run
+any pipeline. For help from the command line, simply call and work from there:
+
+```bash
+mdi
+```
+
+## Run the MDI web server
+
+If desired, run the Stage 2 apps web server as follows:
+
+```bash
+mdi run --help
+mdi run
+```
+
+In a few seconds a web browser will open and you will be ready to 
+load your data and run an associated app.
---
+++ install.sh
@@ -0,0 +1,146 @@
+#---------------------------------------------------------------------------
+# MDI command line installation utility
+#---------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------
+# ensure that we are working in an 'mdi' directory that contains this script
+# -----------------------------------------------------------------------
+export MDI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [[ "$MDI_DIR" != */mdi ]]; then
+    echo
+    echo "ERROR - this script must be run from within an 'mdi' folder than contains it"
+    echo
+    exit 1
+fi
+cd $MDI_DIR
+
+# -----------------------------------------------------------------------
+# prompt for the requested installation action
+# -----------------------------------------------------------------------
+echo
+echo "Welcome to the Michigan Data Interface installation utility."
+echo
+echo "What would you like to install?"
+echo
+echo "  1 - MDI Stage 1 pipelines only; Stage2 apps will be skipped"
+echo "  2 - both Stage 1 pipelines and Stage 2 apps (requires R in PATH)"
+echo "  3 - exit and do nothing"
+echo
+echo "Please note that Stage 2 apps installation takes many minutes."
+echo "Select option 1 if you do not intend to use interactive web tools"
+echo "from this MDI installation."
+echo
+echo "For details, see: https://midataint.github.io/"
+echo
+echo "Select an action by its number: "
+read ACTION_NUMBER
+echo
+
+# -----------------------------------------------------------------------
+# install Stage 1 pipelines only; R is not required
+# -----------------------------------------------------------------------
+if [ "$ACTION_NUMBER" = "1" ]; then
+
+    # initialize the MDI directory tree
+    echo "initializing the MDI file tree"
+    mkdir -p config # populated below
+    mkdir -p data   # unused, this is a pipelines-only installation
+    mkdir -p environments # populated by Stage 1 pipelines conda create
+    mkdir -p frameworks/definitive      # populated below
+    mkdir -p frameworks/developer-forks # unused, pipelines-only installation assumed to be an end user
+    mkdir -p library   # unused, this is a pipelines-only installation
+    mkdir -p remote    # populated below
+    mkdir -p resources # populated by user as needed
+    mkdir -p sessions  # unused, this is a pipelines-only installation
+    mkdir -p suites/definitive      # populated by user via mdi add or other means
+    mkdir -p suites/developer-forks # unused, pipelines-only installation assumed to be an end user
+
+    # clone/pull the mdi-manager repository to access its file resources
+    echo "cloning/updating the mdi-manager repository"
+    function updateRepo {
+        PARENT_FOLDER=$1
+        REPO=$2
+        URL=$3
+        CHECKOUT=$4
+        FOLDER=$PARENT_FOLDER/$REPO
+        if [ -d $FOLDER ]; then
+            cd $FOLDER
+            git checkout main
+            git pull
+        else
+            cd $PARENT_FOLDER
+            git clone $URL
+            cd $REPO
+        fi
+        if [ "$CHECKOUT" = "latest" ]; then
+            CHECKOUT=`git tag -l --sort=-v:refname | head -n1` # the latest tagged version
+        fi
+        echo "checking out $CHECKOUT"
+        git checkout $CHECKOUT
+        cd $MDI_DIR
+    }
+    MDI_MANAGER=mdi-manager
+    updateRepo $MDI_DIR $MDI_MANAGER https://github.com/MiDataInt/$MDI_MANAGER.git main
+
+    # initialize the installation config files if missing, but do not overwrite existing
+    echo "initializing config files"
+    INST_CONFIG=$MDI_MANAGER/inst/config
+    function cpConfigFile {
+        SOURCE=$INST_CONFIG/$1
+        DESTINATION=config/$1
+        if [ ! -e $DESTINATION ]; then cp $SOURCE $DESTINATION; fi
+    }
+    CONFIG_FILES=`ls -1 $INST_CONFIG`
+    for CONFIG_FILE in $CONFIG_FILES; do cpConfigFile $CONFIG_FILE; done
+
+    # initialize/update the remote server scripts
+    echo "initializing remote scripts"
+    cp -f $MDI_MANAGER/inst/remote/* remote 
+
+    # initialize/update the mdi command line utility   
+    echo "initializing 'mdi' command line utility"
+    cp -f $MDI_MANAGER/inst/mdi mdi   
+    chmod ug+x mdi 
+
+    # add MDI_DIR to PATH via ~/.bashrc; do not overwrite if already present
+    echo "checking for mdi directory in PATH"
+    head="# >>> mdi initialize >>>"
+    notice="# !! Contents within this block are managed by 'mdi initialize' !!"
+    path='export PATH='$MDI_DIR':$PATH'
+    tail="# <<< mdi initialize <<<"
+    payload="$head\n$notice\n$path\n$tail"
+    bashRcFile=~/.bashrc
+    bashRcBackup=$bashRcFile.mdi-backup
+    bashRcContents=""
+    match=""
+    if [ -e $bashRcFile ]; then 
+        cp $bashRcFile $bashRcBackup
+        bashRcContents=`sed 's/\r//g' $bashRcFile`
+        match=`grep "$head" $bashRcFile`
+    fi
+    if [ "$match" = "" ]; then
+        echo "adding mdi directory to PATH via ~/.bashrc"
+        bashRcContents="$bashRcContents\n\n$payload"
+        echo -e $bashRcContents | sed 's/\n\n\n/\n\n/g' > $bashRcFile
+    fi
+
+    # clone/pull the definitive pipelines framework repostory
+    # apps framework not needed as this is a pipelines-only installation
+    echo "cloning/updating the mdi-pipelines-framework repository"
+    PIPELINES_FRAMEWORK=mdi-pipelines-framework
+    FRAMEWORKS_DIR=$MDI_DIR/frameworks/definitive
+    updateRepo $FRAMEWORKS_DIR $PIPELINES_FRAMEWORK https://github.com/MiDataInt/$PIPELINES_FRAMEWORK.git latest
+
+    # initialize the pipelines jobManager
+    JOB_MANAGER_DIR=$FRAMEWORKS_DIR/$PIPELINES_FRAMEWORK/job_manager
+    perl $JOB_MANAGER_DIR/initialize.pl $MDI_DIR
+
+# -----------------------------------------------------------------------
+# Stage 2 apps requested; use full mdi::install() from the mdi-manager R package
+# -----------------------------------------------------------------------
+elif [ "$ACTION_NUMBER" = "2" ]; then
+    CRAN_REPO=https://repo.miserver.it.umich.edu/cran/
+    Rscript -e "install.packages('remotes', repos = '$CRAN_REPO')"  
+    Rscript -e "remotes::install_github('MiDataInt/mdi-manager')"  
+    Rscript -e "mdi::install('$MDI_DIR', confirm = FALSE)"
+fi
